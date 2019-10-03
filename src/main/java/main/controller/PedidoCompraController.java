d560cedb6dd9bf6863bdfd84d262ed808ed8b818
Elivelton Douglas Becker <38963615+SrYoshimaa@users.noreply.github.com>
2019-10-03 20:15:24 -0300

    PedidoCompra Insertion

--- src/main/java/main/controller/PedidoCompraController.java
+++ src/main/java/main/controller/PedidoCompraController.java
@@ -1,5 +1,50 @@
 package main.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import main.models.PedidoCompra;
+import main.service.PedidoCompraService;
+
+@Controller
+@RequestMapping("/pedidocompra")
 public class PedidoCompraController {
+    @Autowired
+    private PedidoCompraService pedidoCompraService;
+
+    @GetMapping
+    public ModelAndView index(){
+        List<PedidoCompra> lista = pedidoCompraService.getAll();
+        return new ModelAndView("pedidocompra/index", "pedidocompras", lista);
+    }
+    @GetMapping("/novo")
+    public ModelAndView createForm(@ModelAttribute PedidoCompra pedidoCompra){
+        pedidoCompraService.save(pedidoCompra);
+        return new ModelAndView("redirect:/pedidocompra");
+    }
+    @PostMapping(params="form")
+    public ModelAndView save(@Valid PedidoCompra pedidoCompra){
+        pedidoCompraService.save(pedidoCompra);
+        return new ModelAndView("redirect:/pedidocompra");
+    }
+    @GetMapping(value="/edit/{id}")
+    public ModelAndView edit(@PathVariable("id") PedidoCompra pedidoCompra){
+        return new ModelAndView("pedidocompra/form", "pedidocompra", pedidoCompra);
+    }
+    @GetMapping(value="delete/{id}")
+    public ModelAndView delete(@PathVariable("id") PedidoCompra pedidoCompra){
+        pedidoCompraService.delete(pedidoCompra);
+        return new ModelAndView("redirect:/pedidocompra");
+    }
 
 }
--- src/main/java/main/service/PedidoCompraService.java
+++ src/main/java/main/service/PedidoCompraService.java
@@ -1,5 +1,14 @@
 package main.service;
 
-public interface PedidoCompraService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import main.models.PedidoCompra;
+
+@Service
+public interface PedidoCompraService {
+    List<PedidoCompra> getAll();
+    void save(PedidoCompra pedidoCompra);
+    void delete(PedidoCompra pedidoCompra);
 }
