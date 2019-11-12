7a8798c7d53572de6cc4ae2865d08c818f4e045c
Elivelton Douglas Becker <38963615+SrYoshimaa@users.noreply.github.com>
2019-11-11 21:43:17 -0300

    Adicionar muitas alterações atrasadas

--- src/main/java/main/models/Usuario.java
+++ src/main/java/main/models/Usuario.java
@@ -15,7 +15,9 @@ public class Usuario {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
-	private String descricao;
+	private String username;
+	private String password;
+	private String role;
 	
 	@OneToMany
 	private List<PedidoVenda> pedidovenda = new ArrayList<>();
@@ -33,13 +35,24 @@ public class Usuario {
 	public void setId(long id) {
 		this.id = id;
 	}
-	public String getDescricao() {
-		return descricao;
+	public String getUsername() {
+		return username;
 	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
+	public void setUsername(String username) {
+		this.username = username;
 	}
-	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
 	
 
 }
