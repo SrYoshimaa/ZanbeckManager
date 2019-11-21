ca22436f9f001316e78d18928052113a196b0530
Elivelton Douglas Becker <38963615+SrYoshimaa@users.noreply.github.com>
2019-11-21 10:40:50 -0300

    Various altereichons

--- src/main/java/main/WebSecurityConfig.java
+++ src/main/java/main/WebSecurityConfig.java
@@ -15,64 +15,64 @@ import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 import main.service.impl.MyUserDetailsService;
- 
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private MyUserDetailsService userDetailsService;
- 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
-    }
- 
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider
-        = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
-    }
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
- 
-        http
-        .csrf().disable()
-        .authorizeRequests()
-        .anyRequest().authenticated()
-        .and()
-        .formLogin()
-        .loginPage("/login")
-        .permitAll()
-        .and()
-        .logout()
-        .logoutUrl("/logout")
-        .logoutSuccessUrl("/login")
-        .invalidateHttpSession(true)
-        .permitAll();
-    }
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        String[] resources = new String[]{
-                "/webjars/**","/include/**",
-                "/css/**","/icons/**","/image/**","/js/**","/layer/**","/api/**"
-        };
-        web.ignoring().antMatchers(resources);
-        web.httpFirewall(allowUrlEncodedSlashHttpFirewall());
-    }
-    @Bean
-    public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
-        StrictHttpFirewall firewall = new StrictHttpFirewall();
-        firewall.setAllowUrlEncodedSlash(true);
-        firewall.setAllowSemicolon(true);
-        return firewall;
-    }
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
- 
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider());
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider
+		= new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService);
+		authProvider.setPasswordEncoder(passwordEncoder());
+		return authProvider;
+	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http
+		.csrf().disable()
+		.authorizeRequests()
+		.anyRequest().authenticated()
+		.and()
+		.formLogin()
+		.loginPage("/login")
+		.permitAll()
+		.and()
+		.logout()
+		.logoutUrl("/logout")
+		.logoutSuccessUrl("/login")
+		.invalidateHttpSession(true)
+		.permitAll();
+	}
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		String[] resources = new String[]{
+				"/webjars/**","/include/**",
+				"/css/**","/icons/**","/image/**","/js/**","/layer/**","/api/**"
+		};
+		web.ignoring().antMatchers(resources);
+		web.httpFirewall(allowUrlEncodedSlashHttpFirewall());
+	}
+	@Bean
+	public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
+		StrictHttpFirewall firewall = new StrictHttpFirewall();
+		firewall.setAllowUrlEncodedSlash(true);
+		firewall.setAllowSemicolon(true);
+		return firewall;
+	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
 }
